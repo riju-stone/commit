dd12266d1d43e0b426bf4ec05a691e7b817b4924
Arighna Chakraborty <riju23chakra@gmail.com>
2026-01-16 01:17:48 +0530

    feat: text & color floating toolbar

--- src/views/whiteboard.tsx
+++ src/views/whiteboard.tsx
@@ -1,8 +1,28 @@
 import { motion } from "motion/react"
 import { Editor, FillStyle, Shape, Text, Box, HorzAlign, VertAlign } from "@dgmjs/core"
-import { DGMEditor } from "@dgmjs/react"
+import { DGMEditor, TiptapEditor } from "@dgmjs/react"
 import WhiteboardToolbarComponent from "../components/custom/whiteboard/toolbar"
 import { useState, useRef, useEffect, useCallback } from "react"
+import {
+  AlignCenterIcon,
+  AlignLeftIcon,
+  AlignRightIcon,
+  BoldIcon,
+  ItalicIcon,
+  ListIcon,
+  ListOrderedIcon,
+  PaletteIcon,
+  StrikethroughIcon,
+  UnderlineIcon,
+} from "lucide-react";
+import FloatingToolbar from "@/components/custom/whiteboard/floating-toolbar"
+import { Popover, PopoverTrigger, PopoverContent } from "@/components/ui/popover"
+import { Button } from "@/components/ui/button"
+import { ColorPalette, simplePalette } from "@/components/custom/whiteboard/color-pallete"
+import { Toggle } from "@/components/ui/toggle"
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group"
+
+const DEFAULT_FONT_SIZE = 16;
 
 const WHITEBOARD_VIEW_ANIMATION = {
   initial: {
@@ -62,10 +82,162 @@ function DottedGridOverlay({ origin, scale }: DottedGridOverlayProps) {
   );
 }
 
+export function TextInplaceEditorToolbar({
+  tiptapEditor,
+  darkMode,
+  shape,
+}: {
+  tiptapEditor: TiptapEditor;
+  darkMode: boolean;
+  shape: Box | null;
+}) {
+  const [state, setState] = useState({
+    fontSize: DEFAULT_FONT_SIZE.toString(),
+    bold: false,
+    italic: false,
+    underline: false,
+    strike: false,
+    bulletList: false,
+    orderedList: false,
+    textAlign: "left",
+  });
+
+  useEffect(() => {
+    tiptapEditor.on("transaction", (tr) => {
+      setState({
+        fontSize:
+          tr.editor.getAttributes("textStyle").fontSize?.trim().slice(0, -2) ??
+          shape?.fontSize.toString() ??
+          DEFAULT_FONT_SIZE.toString(),
+        bold: tr.editor.isActive("bold"),
+        italic: tr.editor.isActive("italic"),
+        underline: tr.editor.isActive("underline"),
+        strike: tr.editor.isActive("strike"),
+        bulletList: tr.editor.isActive("bulletList"),
+        orderedList: tr.editor.isActive("orderedList"),
+        textAlign: tr.editor.getAttributes("paragraph").textAlign || "left",
+      });
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div className="h-8 rounded-md p-0 gap-0 flex items-center bg-[rgba(34, 40, 49, 0.8)] backdrop-blur-2xl drop-shadow-lg border-2">
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button variant="ghost"
+            className="h-8 w-8 px-2 rounded-r-none"
+          >
+            <PaletteIcon size={16} />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent
+          sideOffset={8}
+          className="w-fit bg-slate-950 z-10000"
+        >
+          <ColorPalette
+            theme={darkMode ? "dark" : "light"}
+            palette={simplePalette}
+            onClick={(value: any) => {
+              (tiptapEditor.chain().focus() as any).setColor(value).run();
+            }}
+          />
+        </PopoverContent>
+      </Popover>
+      <Toggle
+        className="w-8 h-8 px-2 border-y rounded-none"
+        pressed={state.bold}
+        onPressedChange={() =>
+          (tiptapEditor.chain().focus() as any).toggleBold().run()
+        }
+      >
+        <BoldIcon size={16} />
+      </Toggle>
+      <Toggle
+        className="w-8 h-8 px-2 border-y rounded-none"
+        pressed={state.italic}
+        onPressedChange={() =>
+          (tiptapEditor.chain().focus() as any).toggleItalic().run()
+        }
+      >
+        <ItalicIcon size={16} />
+      </Toggle>
+      <Toggle
+        className="w-8 h-8 px-2 border-y rounded-none"
+        pressed={state.underline}
+        onPressedChange={() =>
+          (tiptapEditor.chain().focus() as any).toggleUnderline().run()
+        }
+      >
+        <UnderlineIcon size={16} />
+      </Toggle>
+      <Toggle
+        className="w-8 h-8 px-2 border-y rounded-none"
+        pressed={state.strike}
+        onPressedChange={() =>
+          (tiptapEditor.chain().focus() as any).toggleStrike().run()
+        }
+      >
+        <StrikethroughIcon size={16} />
+      </Toggle>
+      <Toggle
+        className="w-8 h-8 px-2 border-l border-y rounded-none"
+        pressed={state.bulletList}
+        onPressedChange={() =>
+          (tiptapEditor.chain().focus() as any).toggleBulletList().run()
+        }
+      >
+        <ListIcon size={16} />
+      </Toggle>
+      <Toggle
+        className="w-8 h-8 px-2 border-y rounded-none"
+        pressed={state.orderedList}
+        onPressedChange={() =>
+          (tiptapEditor.chain().focus() as any).toggleOrderedList().run()
+        }
+      >
+        <ListOrderedIcon size={16} />
+      </Toggle>
+      <ToggleGroup
+        type="single"
+        className="border-none h-8 gap-0"
+        value={state.textAlign}
+        onValueChange={(value) => {
+          (tiptapEditor.chain().focus() as any).setTextAlign(value).run();
+        }}
+      >
+        <ToggleGroupItem
+          size="sm"
+          value="left"
+          className="w-8 h-8 px-2 border-l border-y rounded-none"
+        >
+          <AlignLeftIcon size={16} />
+        </ToggleGroupItem>
+        <ToggleGroupItem
+          size="sm"
+          value="center"
+          className="w-8 h-8 px-2 border-y rounded-none"
+        >
+          <AlignCenterIcon size={16} />
+        </ToggleGroupItem>
+        <ToggleGroupItem
+          size="sm"
+          value="right"
+          className="w-8 h-8 px-2 border-y rounded-none"
+        >
+          <AlignRightIcon size={16} />
+        </ToggleGroupItem>
+      </ToggleGroup>
+    </div>
+  );
+}
+
 function WhiteboardView() {
   const [editor, setEditor] = useState<Editor | null>(null);
   const [activeHandler, setActiveHandler] = useState<string>('Select');
   const [gridState, setGridState] = useState({ origin: [0, 0], scale: 1 });
+  const [tiptapEditor, setTiptapEditor] = useState<any>(null);
+  const [editingText, setEditingText] = useState<Box | null>(null);
   const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -128,6 +300,7 @@ function WhiteboardView() {
     <div className="grow w-full h-screen bg-transparent flex items-center justify-center overflow-hidden text-white" ref={containerRef}>
       <motion.div layout className="w-full h-screen relative" variants={WHITEBOARD_VIEW_ANIMATION} initial="initial" animate="visible" exit="initial">
         <DGMEditor
+          darkMode={true}
           className="w-full h-screen [&_canvas]:w-screen [&_canvas]:h-full"
           options={{ canvasColor: "rgba(34, 40, 49, 1)" }}
           onMount={handleEditorMount}
@@ -135,6 +308,20 @@ function WhiteboardView() {
           onActiveHandlerChange={(handler) => setActiveHandler(handler)}
           onScroll={handleScroll}
           onZoom={handleZoom}
+          floatingToolbar={<FloatingToolbar />}
+          textInplaceEditorToolbar={
+            <TextInplaceEditorToolbar
+              darkMode={true}
+              tiptapEditor={tiptapEditor}
+              shape={editingText}
+            />
+          }
+          onTextInplaceEditorMount={(tiptapEditor) => {
+            setTiptapEditor(tiptapEditor);
+          }}
+          onTextInplaceEditorOpen={(shape) => {
+            setEditingText(shape as Box);
+          }}
         />
         <DottedGridOverlay
           origin={gridState.origin}
